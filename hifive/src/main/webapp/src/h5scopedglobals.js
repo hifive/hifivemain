9b7aa18f7e055358abf295cd4fec79420f24dcb6
mtakeuchi <takeuchi.makoto@ns-sol.co.jp>
2012-05-10 16:07:09 +0900

    IE8だとArray.indexOfが未定義でエラーになるため修正。

--- hifive/src/main/webapp/src/h5scopedglobals.js
+++ hifive/src/main/webapp/src/h5scopedglobals.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * hifive
  */
 /* h5scopedglobals */
@@ -142,7 +142,7 @@ function getRegex(target) {
 		return target;
 	}
 	var str = '';
-	if (target.indexOf('*') !== -1) {
+	if ($.inArray('*', target) !== -1) {
 		var array = $.map(target.split('*'), function(n) {
 			return escapeRegex(n);
 		});
