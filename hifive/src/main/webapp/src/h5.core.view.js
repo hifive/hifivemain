942c2244a6b73325eddc0108653bb2848ad4e4eb
fukudayasuo <fukuda.yasuo@ns-sol.co.jp>
2012-07-05 13:04:42 +0900

    fwLoggerの宣言がdel bigin-endの外に書かれていたのを修正

--- hifive/src/main/webapp/src/h5.core.view.js
+++ hifive/src/main/webapp/src/h5.core.view.js
@@ -101,10 +101,9 @@
 	// Development Only
 	// =============================
 
-	var fwLogger = h5.log.createLogger('h5.core.view');
-
 	/* del begin */
-
+	// TODO Minify時にプリプロセッサで削除されるべきものはこの中に書く
+	var fwLogger = h5.log.createLogger('h5.core.view');
 	/* del end */
 
 	// =========================================================================
