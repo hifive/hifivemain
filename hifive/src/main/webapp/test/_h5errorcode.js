47cf8bab92b1a3e668437ec04cb3634da6a5e973
simdy <shimoda.osamu@ns-sol.co.jp>
2012-08-06 11:02:08 +0900

    エラー定数定義ファイルを追加

---
+++ hifive/src/main/webapp/test/_h5errorcode.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 NS Solutions Corporation, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * hifive
+ */
+
+h5.u.obj.expose('ERRCODE.h5.core.data', {
+	ERR_CODE_INVALID_MANAGER_NAME: 15000,
+	ERR_CODE_INVALID_TYPE: 15001,
+	ERR_CODE_DEPEND_PROPERTY: 15002,
+	ERR_CODE_NO_EVENT_TARGET: 15003,
+	ERR_CODE_INVALID_DESCRIPTOR: 15004,
+	ERR_CODE_INVALID_MANAGER_NAMESPACE: 15005,
+	ERR_CODE_INVALID_DATAMODEL_NAME: 15006,
+	ERR_CODE_NO_ID: 15007,
+	ERR_CODE_REGISTER_TARGET_ALREADY_EXIST: 15008,
+	ERR_CODE_ID_MUST_BE_STRING: 15010,
+});
