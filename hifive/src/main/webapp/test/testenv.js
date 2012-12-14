c62769e3590e27595a0a4549e950b535636a8418
fukudayasuo <fukuda.yasuo@ns-sol.co.jp>
2012-12-14 11:12:12 +0900

    H5_ENV_TESTで定義されていないものをリクエストパラメータから読むという仕様に変更。 # 先ほどのコミットのH5_ENV_TEST_ADD案は削除

--- hifive/src/main/webapp/test/testenv.js
+++ hifive/src/main/webapp/test/testenv.js
@@ -1,38 +1,35 @@
 $(function() {
-	// テスト環境オブジェクト。既に作成されていれば、パラメータからは読み込まない。
-	if (!window.H5_TEST_ENV) {
-		// リクエストパラメータから取得して生成
-		var paramsArray = window.location.search.substring(1).split('&');
+	// テスト環境オブジェクトの作成。
+	// リクエストパラメータから取得して生成
+	var paramsArray = window.location.search.substring(1).split('&');
 
-		var l = paramsArray.length;
-		if (l === 0) {
-			// FILTER_ENVもリクエストパラメータも無ければ全テストを実行するので、フィルタは掛けない。
-			return;
-		}
-		// リクエストパラメータの値を使用する
-		window.H5_TEST_ENV = {};
-		for ( var i = 0; i < l; i++) {
-			var keyVal = paramsArray[i].split('=');
-			var namespace = keyVal[0];
-			var val = keyVal[1];
+	// リクエストパラメータから取得したオブジェクト
+	var envByParam = {};
 
-			var names = namespace.split('.');
-			var ret = window.H5_TEST_ENV;
-			for ( var j = 0, len = names.length; j < len - 1; j++) {
-				if (ret[names[j]] == null) { // nullまたはundefjnedだったら辿らない
-					ret[names[j]] = {};
-				}
-				ret = ret[names[j]];
+	var l = paramsArray.length;
+	if (l === 0) {
+		// FILTER_ENVもリクエストパラメータも無ければ全テストを実行するので、フィルタは掛けない。
+		return;
+	}
+	for ( var i = 0; i < l; i++) {
+		var keyVal = paramsArray[i].split('=');
+		var namespace = keyVal[0];
+		var val = keyVal[1];
+
+		var names = namespace.split('.');
+		var ret = envByParam;
+		for ( var j = 0, len = names.length; j < len - 1; j++) {
+			if (ret[names[j]] == null) { // nullまたはundefjnedだったら辿らない
+				ret[names[j]] = {};
 			}
-			ret[names[len - 1]] = val;
+			ret = ret[names[j]];
 		}
+		ret[names[len - 1]] = val;
 	}
+	// H5_TEST_ENVが既に定義されていれば、定義オブジェクト優先でマージする
+	// 定義されていない場合はリクエストパラメータから取得したオブジェクトをそのまま使用する
+	window.H5_TEST_ENV = window.H5_TEST_ENV ? $.extend(envByParam, window.H5_TEST_ENV) : envByParam;
 
-	// H5_TEST_ENV_ADDがあればマージ。
-	// H5_TEST_ENV_ADDは、リクエストパラメータとオブジェクト両方で指定したい時に記述するオブジェクト。
-	if (window.H5_TEST_ENV_ADD) {
-		$.extend(window.H5_TEST_ENV, window.H5_TEST_ENV_ADD);
-	}
 
 	// 環境設定
 	// dummygeoが設定されていればnavigator.geolocationの持つメソッドをダミーで上書く(オリジナルはprototypeメソッドなので、メンバとして上書く)
