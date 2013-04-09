c5ea3795171e015153eb3b2d5bdad01dc32b9e02
fukudayasuo <fukuda.yasuo@ns-sol.co.jp>
2013-04-09 11:03:28 +0900

    テストのリファクタ。 ・不要な待機時間の見直し ・jQuery1.6.x系のテストにフィルタタグを追加 ・テスト名の変更

--- hifive/src/main/webapp/test/h5.async.js
+++ hifive/src/main/webapp/test/h5.async.js
@@ -153,90 +153,86 @@ $(function() {
 		strictEqual(ret, 3, 'then()で登録したprogressCallbackは動作するか');
 	});
 
-	asyncTest('pipeのdoneコールバックがPromiseを返す場合、pipeの実行がPromiseの完了を待っているか (jQuery1.6.x用テスト)', 6,
-			function() {
+	asyncTest(
+			'[jquery#1.7-]pipeのdoneコールバックがPromiseを返す場合、pipeの実行がPromiseの完了を待っているか (jQuery1.6.x用テスト)',
+			6, function() {
 				var count = 1;
 
 				h5.async.deferred().resolve().pipe(function() {
 					var dfd = $.Deferred();
-					var time = 1200;
 
 					setTimeout(function() {
-						equal(count++, 1, '1番目に実行されること。');
+						equal(count++, 1, '1番目に実行されること。(pipe1)');
 						dfd.resolve();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).done(function() {
-					equal(count++, 2, '1番目のpipeが返すPromiseがresolveされたので、doneコールバックが実行されること。');
+					equal(count++, 2, '2番目に実行されること。(pipe1のdoneハンドラ)');
 				}).pipe(function() {
 					var dfd = $.Deferred();
-					var time = 600;
 
 					setTimeout(function() {
-						equal(count++, 3, '3番目に実行されること。');
+						equal(count++, 3, '3番目に実行されること。(pipe2)');
 						dfd.resolve();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).done(function() {
-					equal(count++, 4, '2番目のpipeが返すPromiseがresolveされたので、doneコールバックが実行されること。');
+					equal(count++, 4, '4番目に実行されること。(pipe2のdoneハンドラ)');
 				}).pipe(function() {
 					var dfd = $.Deferred();
-					var time = 800;
 
 					setTimeout(function() {
-						equal(count++, 5, '5番目に実行されること。');
+						equal(count++, 5, '5番目に実行されること。(pipe3)');
 						dfd.resolve();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).done(function() {
-					equal(count++, 6, '3番目のpipeが返すPromiseがresolveされたので、doneコールバックが実行されること。');
+					equal(count++, 6, '6番目に実行されること。(pipe3のdoneハンドラ)');
 					start();
 				});
 			});
 
-	asyncTest('pipeのfailコールバックがPromiseを返す場合、pipeの実行がPromiseの完了を待っているか (jQuery1.6.x用テスト)', 6,
-			function() {
+	asyncTest(
+			'[jquery#1.7-]pipeのfailコールバックがPromiseを返す場合、pipeの実行がPromiseの完了を待っているか (jQuery1.6.x用テスト)',
+			6, function() {
 				var count = 1;
 
 				h5.async.deferred().reject().pipe(null, function() {
 					var dfd = $.Deferred();
-					var time = 1200;
 
 					setTimeout(function() {
-						equal(count++, 1, '1番目に実行されること。');
+						equal(count++, 1, '1番目に実行されること。(pipe1)');
 						dfd.reject();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).fail(function() {
-					equal(count++, 2, '1番目のpipeが返すPromiseがrejectされたので、failコールバックが実行されること。');
+					equal(count++, 2, '2番目に実行されること。(pipe1のfailハンドラ)');
 				}).pipe(null, function() {
 					var dfd = $.Deferred();
-					var time = 600;
 
 					setTimeout(function() {
-						equal(count++, 3, '3番目に実行されること。');
+						equal(count++, 3, '3番目に実行されること。(pipe2)');
 						dfd.reject();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).fail(function() {
-					equal(count++, 4, '2番目のpipeが返すPromiseがrejectされたので、failコールバックが実行されること。');
+					equal(count++, 4, '4番目に実行されること。(pipe2のfailハンドラ)');
 				}).pipe(null, function() {
 					var dfd = $.Deferred();
-					var time = 800;
 
 					setTimeout(function() {
-						equal(count++, 5, '5番目に実行されること。');
+						equal(count++, 5, '5番目に実行されること。(pipe3)');
 						dfd.reject();
-					}, time);
+					}, 0);
 
 					return dfd.promise();
 				}).fail(function() {
-					equal(count++, 6, '3番目のpipeが返すPromiseがrejectされたので、failコールバックが実行されること。');
+					equal(count++, 6, '6番目に実行されること。(pipe3のfailハンドラ)');
 					start();
 				});
 			});
@@ -712,7 +708,7 @@ $(function() {
 	// Body
 	//=============================
 
-	asyncTest('loop', 1, function() {
+	asyncTest('同期', 1, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var p = h5.async.loop(array, function(index, value, loopControl) {
@@ -720,27 +716,33 @@ $(function() {
 		}, 2);
 		p.always(function() {
 			start();
-			strictEqual(ret.join(';'), '0;1;2;3;4;5', 'ちゃんとループしているか');
+			strictEqual(ret.join(';'), '0;1;2;3;4;5', '順番に関数が実行されていること');
 		});
 	});
-	asyncTest('loop 2', 1, function() {
+
+	asyncTest('非同期', 1, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var p = h5.async.loop(array, function(index, value, loopControl) {
 			var dfd = h5.async.deferred();
-			var time = index % 2 === 0 ? 200 : 10;
-			setTimeout(function() {
-				ret.push(value);
-				dfd.resolve();
-			}, time);
-			return dfd.promise();
+			// 奇数なら非同期
+			if (index % 2) {
+				setTimeout(function() {
+					ret.push(value);
+					dfd.resolve();
+				}, 0);
+				return dfd.promise();
+			}
+			// 偶数なら同期
+			ret.push(value);
 		}, 2);
 		p.always(function() {
 			start();
-			strictEqual(ret.join(';'), '0;1;2;3;4;5', 'promiseを返した時に処理を待っているか');
+			strictEqual(ret.join(';'), '0;1;2;3;4;5', '順番に関数が実行されていること');
 		});
 	});
-	asyncTest('loop 3', 2, function() {
+
+	asyncTest('loopControl#stop', 2, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var p = h5.async.loop(array, function(index, value, loopControl) {
@@ -758,21 +760,49 @@ $(function() {
 			ok(false, 'loopControl.stop()でdoneコールバックが呼ばれるか');
 		});
 	});
-	asyncTest('loop 4', 2, function() {
+
+	asyncTest('done', 2, function() {
+		var ret = [];
+		var array = [0, 1, 2, 3, 4, 5];
+		var time = 2;
+		var p = h5.async.loop(array, function(index, value, loopControl) {
+			loopControl.pause();
+			setTimeout(function() {
+				ret.push(value);
+				if (index === 3) {
+					loopControl.stop();
+				} else {
+					loopControl.resume();
+				}
+			}, 0);
+		}, time);
+		p.done(function(data) {
+			start();
+			ok(true, 'loopControl.stop()でdoneコールバックが呼ばれるか');
+			strictEqual(ret.join(';'), '0;1;2;3', 'loopControler.stop()で処理が中断しているか');
+		}).fail(function(data) {
+			start();
+			ok(false, 'loopControl.stop()でdoneコールバックが呼ばれるか');
+		});
+	});
+
+	asyncTest('fail', 2, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var p = h5.async.loop(array, function(index, value, loopControl) {
 			var dfd = h5.async.deferred();
-			var time = index % 2 === 0 ? 200 : 10;
-			setTimeout(function() {
-				ret.push(value);
-				if (index === 3) {
-					dfd.reject();
-				} else {
-					dfd.resolve();
-				}
-			}, time);
-			return dfd.promise();
+			if (index % 2) {
+				setTimeout(function() {
+					ret.push(value);
+					if (index === 3) {
+						dfd.reject();
+					} else {
+						dfd.resolve();
+					}
+				}, 0);
+				return dfd.promise();
+			}
+			ret.push(value);
 		}, 2);
 		p.fail(function() {
 			start();
@@ -783,7 +813,8 @@ $(function() {
 			ok(false, 'ユーザが作成したDeferredがrejectされるとfailコールバックが呼ばれるか');
 		});
 	});
-	asyncTest('loop 5', 1, function() {
+
+	asyncTest('pause(),resume()', 1, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var p = h5.async.loop(array, function(index, value, loopControl) {
@@ -791,14 +822,15 @@ $(function() {
 			setTimeout(function() {
 				ret.push(value);
 				loopControl.resume();
-			}, 100);
+			}, 0);
 		}, 2);
 		p.always(function(data) {
 			start();
 			strictEqual(ret.join(';'), '0;1;2;3;4;5', 'loopControlは動作しているか');
 		});
 	});
-	asyncTest('loop 6', 10, function() {
+
+	asyncTest('progress 同期', 10, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var time = 2;
@@ -816,7 +848,7 @@ $(function() {
 			strictEqual(ret.join(';'), '0;1;2;3;4;5', '基本動作の確認');
 		});
 	});
-	asyncTest('loop 7', 10, function() {
+	asyncTest('progress 非同期', 10, function() {
 		var ret = [];
 		var array = [0, 1, 2, 3, 4, 5];
 		var time = 2;
@@ -825,7 +857,7 @@ $(function() {
 			setTimeout(function() {
 				ret.push(value);
 				loopControl.resume();
-			}, 100);
+			}, 0);
 		}, time);
 		var count = 1;
 		p.progress(function(status) {
@@ -838,36 +870,14 @@ $(function() {
 			strictEqual(ret.join(';'), '0;1;2;3;4;5', '基本動作の確認');
 		});
 	});
-	asyncTest('loop 8', 2, function() {
-		var ret = [];
-		var array = [0, 1, 2, 3, 4, 5];
-		var time = 2;
-		var p = h5.async.loop(array, function(index, value, loopControl) {
-			loopControl.pause();
-			setTimeout(function() {
-				ret.push(value);
-				if (index === 3) {
-					loopControl.stop();
-				} else {
-					loopControl.resume();
-				}
-			}, 100);
-		}, time);
-		p.done(function(data) {
-			start();
-			ok(true, 'loopControl.stop()でdoneコールバックが呼ばれるか');
-			strictEqual(ret.join(';'), '0;1;2;3', 'loopControler.stop()で処理が中断しているか');
-		}).fail(function(data) {
-			start();
-			ok(false, 'loopControl.stop()でdoneコールバックが呼ばれるか');
-		});
-	});
-	asyncTest('loop 9', 3, function() {
+
+	asyncTest('引数チェック', 3, function() {
 		var noArrayObjs = [1, {}, 'aaa'];
-		var time = 2;
 		for ( var i = 0, len = noArrayObjs.length; i < len; i++) {
 			try {
-				h5.async.loop(noArrayObjs[i], function(index, value, loopControl) {}, time);
+				h5.async.loop(noArrayObjs[i], function() {
+				//
+				}, 2);
 			} catch (e) {
 				ok(true, e.message);
 			}
