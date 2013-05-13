bcafdec6246fa5f1dacf3f24e97a241767da1d6e
fukudayasuo <fukuda.yasuo@ns-sol.co.jp>
2013-05-13 15:39:47 +0900

    testFilterのリファクタ

--- hifive/src/main/webapp/test/testFilter.js
+++ hifive/src/main/webapp/test/testFilter.js
@@ -16,16 +16,53 @@
  * hifive
  */
 
-$(function() {
+(function() {
+
+	/**
+	 * フィルタ情報オブジェクト。定義されているフィルタを元にフィルタを掛ける
+	 */
 	var env;
-	// テストフィルタ用オブジェクト
 	if (window.H5_TEST_ENV && window.H5_TEST_ENV.filter) {
 		env = window.H5_TEST_ENV.filter;
 	} else {
 		// H5_TEST_ENV.filterもリクエストパラメータも無ければ全テストを実行するので、フィルタは掛けない。
 		return;
 	}
+
+	/**
+	 * フィルタ実行オブジェクト 追加のフィルタがある場合はここに追加する
+	 */
+	var filters = {
+		build: {
+			check: checkBuildFilter
+		},
+		jquery: {
+			check: checkjQueryFilter
+		},
+		browser: {
+			check: checkBrowserFilter
+		}
+	};
+
+	//------------------------------------------------------------------------------------
+	// チェック関数で使用する共通関数・変数
+	//------------------------------------------------------------------------------------
+	/**
+	 * バージョンがマッチするかどうか判定する。
+	 */
 	function matchVersion(version, envVersionFull) {
+		// ','区切りで複数指定されている場合はorでチェック
+		var versions = version.split(',');
+		for ( var i = 0, l = versions.length; i < l; i++) {
+			if (_matchVersion(versions[i], envVersionFull)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function _matchVersion(version, envVersionFull) {
+		version = $.trim(version);
 		var envVersionFullAry = envVersionFull.split('.');
 		var envMajorVersion = envVersionFullAry[0];
 		// 範囲指定
@@ -73,29 +110,111 @@ $(function() {
 		}
 
 		// 単一指定または複数指定
-		var versions = version.split(',');
-		for ( var i = 0, l = versions.length; i < l; i++) {
-			if (versions[i].indexOf('.') !== -1) {
-				if (envVersionFull.indexOf(versions[i]) === 0) {
-					return true;
-				}
-			} else {
-				if (version == versions[i]) {
-					return true;
-				}
-			}
+		if (envVersionFull.indexOf(version) === 0) {
+			return true;
 		}
 
 		return false;
 	}
 
-	function throughFilter(filtersArray) {
+	//------------------------------------------------------------------------------------
+	// チェック関数
+	//------------------------------------------------------------------------------------
+	/**
+	 * build#xxx 指定されたxxxが現環境(env.build)にマッチするかどうかを返す。
+	 */
+	function checkBuildFilter(buildFilters, stats) {
+		if (!buildFilters) {
+			return false;
+		}
+		for ( var i = 0, l = buildFilters.length; i < l; i++) {
+			if (env.build === buildFilters[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * jquery#xxx 指定されたxxxが現環境(env.jqeury)にマッチするかどうかを返す。
+	 */
+	function checkjQueryFilter(jqueryFilters, stats) {
+		if (!jqueryFilters) {
+			return false;
+		}
+		for ( var i = 0, l = jqueryFilters.length; i < l; i++) {
+			var desc = jqueryFilters[i];
+
+			if (matchVersion(desc, env.jquery)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * browser#xxx 指定されたxxxが現環境(env.browser, env.browserversion)にマッチするかどうかを返す。
+	 */
+	function checkBrowserFilter(browserFilters, stats) {
+		if (!browserFilters) {
+			return false;
+		}
+		// "|"で結合する。余分についた両端の"|"は削除。"|"を区切り記号にして分割し、配列にする。
+		var descs = browserFilters.join('|').replace(/^\||$\|/g, '').split('|');
+		for ( var i = 0, l = descs.length; i < l; i++) {
+			var desc = descs[i].split(':');
+			if ($.trim(desc[0]) !== env.browserprefix) {
+				continue;
+			}
+			if (!matchVersion(desc[1], env.browserversion)) {
+				continue;
+			}
+
+			// ブラウザ、バージョンがマッチ
+
+			if (!desc[2]) { //オプションがない場合はreturn true
+				return true;
+			}
+			var options = desc[2].split('&');
+			// optionによるフィルタ(IEのdocmodeなど)
+			for ( var j = 0, len = options.length; j < len; j++) {
+				var tmp = options[j].split('=');
+				var key = $.trim(tmp[0]);
+				var val = $.trim(tmp[1]);
+				if (key === 'docmode') {
+					// docmodeなら数値で比較
+					if (!matchVersion(val, env.docmode)) {
+						break;
+					}
+				} else if (new String(env[key]).toLowerCase() !== val.toLowerCase()) {
+					// それ以外のオプションは文字列で比較
+					break;
+				}
+			}
+			// opitionが全てマッチしたらtrueを返す
+			if (j === len) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//------------------------------------------------------------------------------------
+	// body
+	//------------------------------------------------------------------------------------
+	/**
+	 * filterの記述からオブジェクトを生成して返す
+	 */
+	function createFilterObj(filtersArray) {
 		var filtersObj = {};
 
 		// []を外して、";"で結合する。余分についた両端の";"は削除。";"を区切り記号にして分割し、配列にする。
 		var filters = filtersArray.join(';').replace(/\[|\]|^;|$;|;;/g, '').split(';');
 		for ( var i = 0, l = filters.length; i < l; i++) {
 			var filter = $.trim(filters[i]);
+			if (filter === '') {
+				continue;
+			}
 
 			// 判定種別が省略されていたらbrowser
 			if (filter.indexOf('#') === -1) {
@@ -113,81 +232,30 @@ $(function() {
 			}
 			filtersObj[tag].push(conditionStr);
 		}
-		return checkBuildFilter(filtersObj['build']) || checkjQueryFilter(filtersObj['jquery'])
-				|| checkBrowserFilter(filtersObj['browser']);
+		return filtersObj;
 	}
 
-	// build#xxx 指定されたxxxが現環境(env.build)にマッチするかどうかを返す。
-	function checkBuildFilter(buildFilters) {
-		if (!buildFilters) {
-			return false;
-		}
-		for ( var i = 0, l = buildFilters.length; i < l; i++) {
-			if (env.build === buildFilters[i]) {
+	/**
+	 * フィルタにマッチするかどうかチェックを行い、マッチしたらtrueを返す。
+	 */
+	function check(filtersObj, state) {
+		for ( var p in filtersObj) {
+			if (filters[p].check(filtersObj[p], state)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	function checkjQueryFilter(jqueryFilters) {
-		if (!jqueryFilters) {
-			return false;
-		}
-		for ( var i = 0, l = jqueryFilters.length; i < l; i++) {
-			var filter = jqueryFilters[i];
-
-			if (matchVersion($.trim(filter), env.jquery)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	function checkBrowserFilter(browserFilters) {
-		if (!browserFilters) {
-			return false;
-		}
-		// "|"で結合する。余分についた両端の"|"は削除。"|"を区切り記号にして分割し、配列にする。
-		var filters = browserFilters.join('|').replace(/^\||$\|/g, '').split('|');
-		for ( var i = 0, l = filters.length; i < l; i++) {
-			var filter = filters[i].split(':');
-			if ($.trim(filter[0]) !== env.browserprefix) {
-				continue;
-			}
-			if (!matchVersion($.trim(filter[1]), env.browserversion)) {
-				continue;
-			}
-
-			// ブラウザ、バージョンがマッチ
-
-			if (!filter[2]) { //オプションがない場合はreturn true
-				return true;
-			}
-			var options = filter[2].split(',');
-			// optionによるフィルタ(IEのdocmodeなど)
-			for ( var j = 0, len = options.length; j < len; j++) {
-				var tmp = options[j].split('=');
-				var key = $.trim(tmp[0]);
-				var val = $.trim(tmp[1]);
-				if (new String(env[key]).toLowerCase() !== val.toLowerCase()) {
-					break;
-				}
-			}
-			// opitionが全てマッチしたらtrueを返す
-			if (j === len) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	QUnit.config.testStart.push(function(stats) {
+	/**
+	 * testStartにpushする、テストフィルタを実行するための関数
+	 */
+	function doFilter(stats) {
 		var current = QUnit.config.current;
 
 		var testConditionDesc = stats.name.match(/^\[.*?\]/);
 		testConditionDesc = testConditionDesc && testConditionDesc[0];
-		var moduleConditionDesc = current.module ? current.module.match(/^\[.*?\]/) : '';
+		var moduleConditionDesc = stats.module ? stats.module.match(/^\[.*?\]/) : '';
 		moduleConditionDesc = moduleConditionDesc && moduleConditionDesc[0];
 
 		// 条件が書かれていないなら何もしない
@@ -195,8 +263,11 @@ $(function() {
 			return;
 		}
 
+		// パースしたオブジェクトの生成
+		var descriptedFilterObj = createFilterObj([testConditionDesc, moduleConditionDesc]);
+
 		// 条件が、現在の環境でマッチするならテストをスルー
-		if (throughFilter([testConditionDesc, moduleConditionDesc])) {
+		if (check(descriptedFilterObj, stats)) {
 			current.testEnvironment.setup = function() {};
 			current.testEnvironment.teardown = function() {};
 			current.callback = function() {
@@ -208,8 +279,10 @@ $(function() {
 			current.name = '<span class="module-name">' + stats.module
 					+ '</span>: <span class="test-name">[テストをスキップしました] ' + stats.name + '</span>';
 		}
-	});
+	}
 
-
-
-});
+	/**
+	 * フィルタをtestStartに追加
+	 */
+	QUnit.config.testStart.push(doFilter);
+})();
