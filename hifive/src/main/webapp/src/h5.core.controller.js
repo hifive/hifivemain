3fbb819c93325269b6b80caf56c614cee34f812e
fukudayasuo <fukuda.yasuo@ns-sol.co.jp>
2014-03-03 17:12:58 +0900

    #249 ロジック定義オブジェクトの不要なクローンをしないようにしました。
    (createLogicにはすでにclone済みのロジック定義オブジェクトが渡されており、__logicContext.logicDefに持たせるときに改めてコピーする必要はありませんでした。)

--- hifive/src/main/webapp/src/h5.core.controller.js
+++ hifive/src/main/webapp/src/h5.core.controller.js
@@ -3023,7 +3023,7 @@
 		logic.deferred = getDeferred;
 		logic.log = h5.log.createLogger(logicName);
 		logic.__logicContext = {
-			logicDef: $.extend(true, {}, logicDefObj)
+			logicDef: logicDefObj
 		};
 		logic.own = own;
 		logic.ownWithOrg = ownWithOrg;
