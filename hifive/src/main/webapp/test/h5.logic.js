2cdd557fd5fb3a5064ba36798eb20d39093681be
fukudayasuo <fukuda.yasuo.2ga@jp.nssol.nssmc.com>
2014-06-17 16:36:36 +0900

    #355 ロジックのテストケースをモジュール分けしました。

--- hifive/src/main/webapp/test/h5.logic.js
+++ hifive/src/main/webapp/test/h5.logic.js
@@ -35,12 +35,12 @@ $(function() {
 	// testutils
 	var clearController = testutils.u.clearController;
 
+	// エラーコード
 	var ERR = ERRCODE.h5.core.controller;
 
 	//=============================
 	// Functions
 	//=============================
-
 	// testutils
 	var cleanAspects = testutils.u.cleanAspects;
 
@@ -52,25 +52,18 @@ $(function() {
 	//=============================
 	// Definition
 	//=============================
-
-	module(
-			"Logic",
-			{
-				setup: function() {
-					$('#qunit-fixture')
-							.append(
-									'<div id="controllerTest" style="display: none;"><div id="controllerResult"></div><input type="button" value="click" /><button id="btn" name="click">btn</button></div>');
-				},
-				teardown: function() {
-					$('#controllerTest').remove();
-					clearController();
-				}
-			});
+	module('Logic __construct', {
+		setup: function() {
+			$('#qunit-fixture').append('<div id="controllerTest"></div>');
+		},
+		teardown: function() {
+			clearController();
+		}
+	});
 
 	//=============================
 	// Body
 	//=============================
-
 	asyncTest('__constructの実行', 3, function() {
 		var myLogic = {
 			__name: 'logic',
@@ -102,7 +95,10 @@ $(function() {
 						ok(this.childController.myLogic.isExecuted,
 								'子コントローラのロジックの__constructがルートコントローラの__constructよりも前に実行されていること');
 					}
-				}).readyPromise.done(start);
+				}).readyPromise.done(function() {
+			clearController();
+			start();
+		});
 	});
 
 	test('__constructが例外を投げる場合', 1, function() {
@@ -146,26 +142,61 @@ $(function() {
 		});
 	});
 
-	test('[build#min]__constructに対してアスペクトが適用されること', 1, function() {
-		var isInterceptorExecuted;
-		var logicAspect = {
-			target: /TestLogic/,
-			interceptors: function(invocation) {
-				isInterceptorExecuted = true;
-				invocation.proceed();
-			}
-		};
-		h5.core.__compileAspects(logicAspect);
-		h5.core.logic({
-			__name: 'TestLogic',
-			__construct: function() {
-			// 何もしない
-			}
-		});
-		ok(isInterceptorExecuted);
-		cleanAspects();
+	//=============================
+	// Definition
+	//=============================
+	module('Logic 異常系', {
+		setup:function(){
+			$('#qunit-fixture').append('<div id="controllerTest"><input type="button"></div>');
+		},
+		teardown: function() {
+			cleanAspects();
+		}
 	});
 
+	//=============================
+	// Body
+	//=============================
+	test('ロジックの循環参照チェックに引っかかるとエラーが発生するか', 1, function() {
+		var test1Logic = {
+			__name: 'Test1Logic'
+		};
+		var test2Logic = {
+			__name: 'Test2Logic',
+
+			test1Logic: test1Logic
+		};
+
+		test1Logic.test2Logic = test2Logic;
+
+		var testController = {
+			__name: 'TestController',
+			test1Logic: test1Logic
+		};
+
+		try {
+			h5.core.controller('#controllerTest', testController);
+		} catch (e) {
+			strictEqual(e.code, ERR.ERR_CODE_LOGIC_CIRCULAR_REF, 'エラーが発生したか');
+		}
+
+	});
+	//=============================
+	// Definition
+	//=============================
+	module('Logicのメソッド', {
+		setup: function() {
+			$('#qunit-fixture').append('<div id="controllerTest"><input type="button"></div>');
+		},
+		teardown: function() {
+			clearController();
+			cleanAspects();
+		}
+	});
+
+	//=============================
+	// Body
+	//=============================
 	asyncTest('this.deferred()は使用できるか', function() {
 		var innerDfd = null;
 		function InnerLogic() {
@@ -257,136 +288,6 @@ $(function() {
 		});
 	});
 
-	asyncTest('[build#min]ロジックのAOPは動作しているか', function() {
-		var result = [];
-		function TestLogic() {
-		//
-		}
-		TestLogic.prototype = {
-			__name: 'TestLogic',
-
-			test: function() {
-				result.push(1);
-			},
-
-			_private: function() {
-				result.push(2);
-			}
-		};
-
-		var logicAspect = {
-			target: /TestLogic/,
-			interceptors: function(invocation) {
-				result.push(0);
-				invocation.proceed();
-			},
-			pointCut: 'te*'
-		};
-
-		var ret1 = '';
-		var ret2 = '';
-		var controllerBase = {
-			__name: 'TestController',
-
-			testLogic: new TestLogic(),
-			'input[type=button] click': function(el, ev) {
-				this.testLogic.test();
-				ret1 = result.join(';');
-				result = [];
-				this.testLogic._private();
-				ret2 = result.join(';');
-			}
-		};
-		h5.core.__compileAspects(logicAspect);
-		var testController = h5.core.controller('#controllerTest', controllerBase);
-		testController.readyPromise.done(function() {
-			start();
-
-			$('#controllerTest input[type=button]').click();
-
-			strictEqual(ret1, '0;1', 'ロジックのパブリックメソッドにインターセプタはかかっているか');
-			strictEqual(ret2, '2', 'ロジックのプライベートメソッドにインターセプタはかかっていないか');
-
-			testController.unbind();
-			cleanAspects();
-		});
-	});
-
-	test('[build#min]h5.core.logic() の動作', function() {
-		var dfd = null;
-		var logger = null;
-		var result = [];
-		function TestLogic() {
-		//
-		}
-		TestLogic.prototype = {
-			__name: 'TestLogic',
-
-			test: function() {
-				dfd = this.deferred();
-				logger = this.log;
-				result.push(1);
-			},
-
-			_private: function() {
-				result.push(2);
-			}
-		};
-
-		var logicAspect = {
-			interceptors: function(invocation) {
-				result.push(0);
-				invocation.proceed();
-			}
-		};
-		h5.core.__compileAspects(logicAspect);
-
-		var logic = h5.core.logic(new TestLogic());
-
-		logic.test();
-		ok(dfd, 'this.deferred() が使えるか');
-		ok(logger, 'this.log が使えるか');
-		strictEqual(result.join(';'), '0;1', 'ロジックの全てのメソッドにインターセプタはかかっているか1');
-		result = [];
-		logic._private();
-		strictEqual(result.join(';'), '0;2', 'ロジックの全てのメソッドにインターセプタはかかっているか2');
-
-		var error = false;
-		try {
-			h5.core.logic(logic);
-		} catch (e) {
-			error = true;
-		}
-		ok(error, 'ロジック化したオブジェクトを再度h5.core.logic() に渡すとエラーが発生するか');
-		cleanAspects();
-
-	});
-
-	test('ロジックの循環参照チェックに引っかかるとエラーが発生するか', 1, function() {
-		var test1Logic = {
-			__name: 'Test1Logic'
-		};
-		var test2Logic = {
-			__name: 'Test2Logic',
-
-			test1Logic: test1Logic
-		};
-
-		test1Logic.test2Logic = test2Logic;
-
-		var testController = {
-			__name: 'TestController',
-			test1Logic: test1Logic
-		};
-
-		try {
-			h5.core.controller('#controllerTest', testController);
-		} catch (e) {
-			strictEqual(e.code, ERR.ERR_CODE_LOGIC_CIRCULAR_REF, 'エラーが発生したか');
-		}
-
-	});
-
 	asyncTest('own()の動作', 4, function() {
 		function Test(callback) {
 			this.callback = callback;
@@ -469,4 +370,139 @@ $(function() {
 		});
 	});
 
+	//=============================
+	// Definition
+	//=============================
+	module('Logic アスペクト', {
+		setup:function(){
+			$('#qunit-fixture').append('<div id="controllerTest"><input type="button"></div>');
+		},
+		teardown: function() {
+			cleanAspects();
+		}
+	});
+
+	//=============================
+	// Body
+	//=============================
+	asyncTest('[build#min]ロジックのAOPは動作しているか', function() {
+		var result = [];
+		function TestLogic() {
+		//
+		}
+		TestLogic.prototype = {
+			__name: 'TestLogic',
+
+			test: function() {
+				result.push(1);
+			},
+
+			_private: function() {
+				result.push(2);
+			}
+		};
+
+		var logicAspect = {
+			target: /TestLogic/,
+			interceptors: function(invocation) {
+				result.push(0);
+				invocation.proceed();
+			},
+			pointCut: 'te*'
+		};
+
+		var ret1 = '';
+		var ret2 = '';
+		var controllerBase = {
+			__name: 'TestController',
+
+			testLogic: new TestLogic(),
+			'input[type=button] click': function(el, ev) {
+				this.testLogic.test();
+				ret1 = result.join(';');
+				result = [];
+				this.testLogic._private();
+				ret2 = result.join(';');
+			}
+		};
+		h5.core.__compileAspects(logicAspect);
+		var testController = h5.core.controller('#controllerTest', controllerBase);
+		testController.readyPromise.done(function() {
+			start();
+
+			$('#controllerTest input[type=button]').click();
+
+			strictEqual(ret1, '0;1', 'ロジックのパブリックメソッドにインターセプタはかかっているか');
+			strictEqual(ret2, '2', 'ロジックのプライベートメソッドにインターセプタはかかっていないか');
+
+			testController.unbind();
+		});
+	});
+
+	test('[build#min]__constructに対してアスペクトが適用されること', 1, function() {
+		var isInterceptorExecuted;
+		var logicAspect = {
+			target: /TestLogic/,
+			interceptors: function(invocation) {
+				isInterceptorExecuted = true;
+				invocation.proceed();
+			}
+		};
+		h5.core.__compileAspects(logicAspect);
+		h5.core.logic({
+			__name: 'TestLogic',
+			__construct: function() {
+			// 何もしない
+			}
+		});
+		ok(isInterceptorExecuted);
+	});
+
+	test('[build#min]h5.core.logic() の動作', function() {
+		var dfd = null;
+		var logger = null;
+		var result = [];
+		function TestLogic() {
+		//
+		}
+		TestLogic.prototype = {
+			__name: 'TestLogic',
+
+			test: function() {
+				dfd = this.deferred();
+				logger = this.log;
+				result.push(1);
+			},
+
+			_private: function() {
+				result.push(2);
+			}
+		};
+
+		var logicAspect = {
+			interceptors: function(invocation) {
+				result.push(0);
+				invocation.proceed();
+			}
+		};
+		h5.core.__compileAspects(logicAspect);
+
+		var logic = h5.core.logic(new TestLogic());
+
+		logic.test();
+		ok(dfd, 'this.deferred() が使えるか');
+		ok(logger, 'this.log が使えるか');
+		strictEqual(result.join(';'), '0;1', 'ロジックの全てのメソッドにインターセプタはかかっているか1');
+		result = [];
+		logic._private();
+		strictEqual(result.join(';'), '0;2', 'ロジックの全てのメソッドにインターセプタはかかっているか2');
+
+		var error = false;
+		try {
+			h5.core.logic(logic);
+		} catch (e) {
+			error = true;
+		}
+		ok(error, 'ロジック化したオブジェクトを再度h5.core.logic() に渡すとエラーが発生するか');
+	});
 });
