9116d950dfe342df3d688bf5b5431f33d5da9ff6
fukudayasuo <fukuda.yasuo.2ga@jp.nssol.nssmc.com>
2014-07-04 10:28:52 +0900

    #372 position.top/leftに、'middle','center'等のキーワード指定を可能にしました。 refleshメソッドを追加しました。位置計算を再度行って表示します。 mordal:trueを指定するとオーバレイを表示するようにしました。

--- hifive/src/main/webapp/src/h5.scene.js
+++ hifive/src/main/webapp/src/h5.scene.js
@@ -30,12 +30,25 @@
 	/** 吹き出し(三角の部分)の大きさ */
 	var ARROW_SIZE = 34;
 
+	/** オーバレイのクラス */
+	var CLASS_OVERLAY = 'h5PopupOverlay';
+
+	/** シーンの位置指定文字列 */
+	var POSITION_CONSTRAINT = {
+		top: 'top',
+		middle: 'middle',
+		bottom: 'bottom',
+		left: 'left',
+		center: 'center',
+		right: 'right'
+	};
+
 
 	// =============================
 	// Development Only
 	// =============================
 
-//	var fwLogger = h5.log.createLogger('h5.scene');
+	//	var fwLogger = h5.log.createLogger('h5.scene');
 
 	/* del begin */
 	//	var FW_LOG_H5_WHEN_INVALID_PARAMETER = 'h5.scene.when: 引数にpromiseオブジェクトでないものが含まれています。';
@@ -69,13 +82,16 @@
 	// Functions
 	// =============================
 	/**
-	 * アニメーション関数
+	 * アニメーションしながら表示する
 	 *
 	 * @param {jQuery} $element アニメーションさせる要素
 	 */
-	function animate($element) {
+	function animateShow($element) {
 		var height = $element.height();
-		$element.css('height', 0);
+		$element.css({
+			height: 0,
+			display: 'block'
+		});
 		$element.addClass('inAnimation');
 		$element.animate({
 			height: height,
@@ -84,6 +100,50 @@
 		$element.promise().done(function() {
 			$element.removeClass('inAnimation');
 		});
+	}
+
+	/**
+	 * アニメーションしながら非表示にする
+	 *
+	 * @param {jQuery} $element アニメーションさせる要素
+	 */
+	function animateHide($element) {
+		$element.addClass('inAnimation');
+		$element.animate({
+			height: 0,
+			opacity: 0
+		}, "slow");
+		$element.promise().done(function() {
+			$element.removeClass('inAnimation');
+			$element.css('display', 'none');
+			$element.css('height', '');
+			removeOverlay();
+		});
+	}
+
+	function showOverlay($root) {
+		// すでに表示済みなら何もしない
+		if ($('.' + CLASS_OVERLAY).length) {
+			return;
+		}
+		var overlay = $('<div>').addClass(CLASS_OVERLAY);
+		// rootがwindowまたはdocumentならbodyに追加
+		($root[0] === window || $root[0] === document ? $(document.body) : $root).append(overlay);
+		function resizeOverlay() {
+			overlay.css({
+				width: $root.innerWidth(),
+				height: $root.innerHeight()
+			});
+		}
+		resizeOverlay();
+		$(window).bind('resize.h5overlay', resizeOverlay);
+		$(window).bind('orientationchange.h5overlay', resizeOverlay);
+	}
+
+	function removeOverlay() {
+		$(window).unbind('resize.h5overlay');
+		$(window).unbind('orientationchange.h5overlay');
+		$('.' + CLASS_OVERLAY).remove();
 	}
 
 	// =========================================================================
@@ -134,81 +194,153 @@
 	}
 	$.extend(Scene.prototype, {
 		show: function(param) {
+			this._showParam = param;
 			param = param || {};
-			this.hide();
-			var $root = $(param.rootElement || document.body);
+			this.hide(false);
+
+			// $rootは位置計算の起点となるエレメント。未指定の場合はwindow
+			var $root = $(param.rootElement || window);
 			var $element = this.$element;
-			var position = param.position;
 			var styles = {
 				position: 'absolute',
 				display: 'block'
 			};
-			$root.append($element);
+
+			// rootElementがwindowまたはdocumentならbodyにappend
+			var $appendTarget = $root[0] === window || $root[0] === document ? $(document.body)
+					: $root;
+			$appendTarget.append($element);
+
+			// 表示
 			$element.css(styles);
 
-			switch(this.type){
-			case 'arrow':
-				if(position){
-					$element.css(position);
-					break;
+			// ------------- 位置計算 ------------- //
+			// positionを見る
+			var position = param.position;
+			var topLeftStyle = {};
+			if (position) {
+				var top = position.top;
+				var left = position.left;
+
+				// topがキーワード指定('top','middle','bottom'の場合
+				if (top === POSITION_CONSTRAINT.middle) {
+					top = $root.scrollTop() + ($root.height() - $element.outerHeight()) / 2;
+				} else if (top === POSITION_CONSTRAINT.top) {
+					top = $root.scrollTop();
+				} else if (top === POSITION_CONSTRAINT.bottom) {
+					top = $root.scrollTop() + $root.height() - $element.outerHeight();
 				}
+
+				// leftがキーワード指定('left','center','right'の場合
+				if (left === POSITION_CONSTRAINT.center) {
+					left = $root.scrollLeft() + ($root.width() - $element.outerWidth()) / 2;
+				} else if (left === POSITION_CONSTRAINT.left) {
+					left = $root.scrollLeft();
+				} else if (left === POSITION_CONSTRAINT.right) {
+					left = $root.scrollLeft() + $root.width() - $element.outerWidth();
+				}
+				topLeftStyle.top = top;
+				topLeftStyle.left = left;
+			}
+
+			switch (this.type) {
+			case 'arrow':
+				// 吹き出しの向き設定
 				var dir = param.direction || 'down';
 				$element.removeClass('up down right left');
 				$element.addClass(dir);
 				// targetから表示位置を取得する
 				// positionまたはtargetはどちらかの指定が必須。
 				var $target = $(param.target);
+				if (!$target.length) {
+					// targetに何も指定されていないなら位置は計算しない
+					break;
+				}
+
+				// targetから位置を計算する。targetが指定されている場合はpositionの設定は無視。
 				var targetW = $target.outerWidth();
 				var targetH = $target.outerHeight();
-				var arrowboxPosition =  $target.offset();
+				topLeftStyle = $target.offset();
 				// $targetと$arrowboxの左上の位置を合わせる
 				if (dir === 'up' || dir === 'down') {
 					// 吹き出しの位置が$targetの真ん中に来るように合わせる
-					arrowboxPosition.left += (targetW - $element.outerWidth()) / 2;
+					topLeftStyle.left += (targetW - $element.outerWidth()) / 2;
 					if (dir === 'up') {
 						// 吹き出し分だけ上に移動
-						arrowboxPosition.top -= $element.outerHeight() + ARROW_SIZE;
+						topLeftStyle.top -= $element.outerHeight() + ARROW_SIZE;
 					} else {
 						// $target分だけ下に移動
-						arrowboxPosition.top += targetH + ARROW_SIZE;
+						topLeftStyle.top += targetH + ARROW_SIZE;
 					}
 				} else {
 					// 吹き出しの位置が$targetの真ん中に来るように合わせる
-					arrowboxPosition.top += (targetH - $element.outerHeight()) / 2;
+					topLeftStyle.top += (targetH - $element.outerHeight()) / 2;
 					if (dir === 'left') {
 						// 吹き出し分だけ左に移動
-						arrowboxPosition.left -= $element.outerWidth() + ARROW_SIZE;
+						topLeftStyle.left -= $element.outerWidth() + ARROW_SIZE;
 					} else {
 						// $target分だけ下に移動
-						arrowboxPosition.left += targetW + ARROW_SIZE;
+						topLeftStyle.left += targetW + ARROW_SIZE;
 					}
 				}
-				$element.css(arrowboxPosition);
 				break;
 			default:
-				$element.css(position);
 			}
-			animate($element);
+			$element.css(topLeftStyle);
+			$element.removeClass('hidden');
+
+			if (param.animation !== false) {
+				// animationはデフォルトでtrue
+				animateShow($element);
+			}
+
+			if (param.mordal) {
+				$element.css('z-index', 10001);
+				// オーバレイを表示
+				showOverlay($root);
+			}
+
+			this._displaying = true;
 			return $element.promise();
 		},
-		hide: function() {
+		hide: function(isAnimation) {
+			if (!this._displaying) {
+				return;
+			}
 			// TODO アニメーションするかどうか引数にとる
 			this.$element.stop(true, true);
-			this.$element.css('display', 'none');
+			if (isAnimation === false) {
+				this.$element.css('display', 'none');
+			} else {
+				animateHide(this.$element);
+			}
+			this.$element.addClass('hidden');
+			this._displaying = false;
 		},
 		setContent: function(content) {
 			this.$content.children().remove();
 			this.$content.append(content);
 		},
-		remove: function(){
+		remove: function() {
 			this.hide();
 			this.$element.remove();
-			delete(scenesMap,this.id);
-			for(var p in this){
+			delete scenesMap[this.id];
+			// nullifyする
+			for ( var p in this) {
 				this[p] = null;
 			}
 		},
-		_animatePromise: null
+		reflesh: function(param) {
+			if (!this._displaying) {
+				// 表示中じゃない場合は何もしない
+				return;
+			}
+			// showの時に渡されたパラメータにrefleshに渡されたパラメータをマージしてshowを呼び出す
+			this.show($.extend(this._showParam, param));
+		},
+		_animatePromise: null,
+		_showParam: null,
+		_displaying: false
 	});
 
 	/**
