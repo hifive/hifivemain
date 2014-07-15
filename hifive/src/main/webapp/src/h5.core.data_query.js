997896a9f98ce278b17f08da714888097b304c4f
fukudayasuo <fukuda.yasuo.2ga@jp.nssol.nssmc.com>
2014-07-15 14:34:15 +0900

    #374 正規表現、日付型、について使用する比較関数を別に作成しました。 比較関数を探し当てられなかった(指定が不正)の時にエラーを出すようにしました。(エラーコードは一時的なもの) 比較関数を追加しました。(between,!=,!in)

--- hifive/src/main/webapp/src/h5.core.data_query.js
+++ hifive/src/main/webapp/src/h5.core.data_query.js
@@ -32,10 +32,10 @@
 
 	var COMPARE_FUNCIONS = {
 		'=': function(value, queryValue) {
-			if (queryValue instanceof RegExp) {
-				return queryValue.test(value);
-			}
 			return value === queryValue;
+		},
+		'!=': function(value, queryValue) {
+			return value !== queryValue;
 		},
 		'<': function(value, queryValue) {
 			return value < queryValue;
@@ -49,10 +49,100 @@
 		'>=': function(value, queryValue) {
 			return value >= queryValue;
 		},
+		'between': function(value, queryValue) {
+			var lower = queryValue[0];
+			var upper = queryValue[1];
+			// 境界値を含む
+			return lower <= value && value <= upper;
+		},
+		'!between': function(value, queryValue) {
+			var lower = queryValue[0];
+			var upper = queryValue[1];
+			// 境界値を含まない
+			return value < lower || upper < value;
+		},
 		'in': function(value, queryValue) {
 			return $.inArray(value, queryValue) !== -1;
+		},
+		'!in': function(value, queryValue) {
+			return $.inArray(value, queryValue) === -1;
 		}
 	};
+
+	var COMPARE_REGEXP_FUNCTIONS = {
+		'=': function(value, queryValue) {
+			return queryValue.test(value);
+		},
+		'!=': function(value, queryValue) {
+			return !queryValue.test(value);
+		}
+	};
+
+	var COMPARE_DATE_FUNCIONS = {
+		'=': function(value, queryValue) {
+			return value.getTime() === queryValue.getTime();
+		},
+		'<': function(value, queryValue) {
+			return value.getTime() < queryValue.getTime();
+		},
+		'>': function(value, queryValue) {
+			return value.getTime() > queryValue.getTime();
+		},
+		'<=': function(value, queryValue) {
+			return value.getTime() <= queryValue.getTime();
+		},
+		'>=': function(value, queryValue) {
+			return value.getTime() >= queryValue.getTime();
+		},
+		'between': function(value, queryValue) {
+			var lower = queryValue[0].getTime();
+			var upper = queryValue[1].getTime();
+			var valueTime = value.getTime();
+			// 境界値を含む
+			return lower <= valueTime && valueTime <= upper;
+		},
+		'!between': function(value, queryValue) {
+			var lower = queryValue[0].getTime();
+			var upper = queryValue[1].getTime();
+			var valueTime = value.getTime();
+			// 境界値を含まない
+			return valueTime < lower || upper < valueTime;
+		},
+		'in': function(value, queryValue) {
+			var valueTime = value.getTime();
+			for (var i = 0; i < l; i++) {
+				if (valueTime === queryValue[i].getTime()) {
+					return true;
+				}
+			}
+			return false;
+		},
+		'!in': function(value, queryValue) {
+			var valueTime = value.getTime();
+			for (var i = 0; i < l; i++) {
+				if (valueTime === queryValue[i].getTime()) {
+					return false;
+				}
+			}
+			return true;
+		},
+	};
+
+	// -------------------------------
+	// エラーコード
+	// -------------------------------
+	// TODO エラーコードの採番は決まってから適切な番号にする
+	/** 指定された比較関数がない */
+	var ERR_CODE_NO_COMPARE_FUNCTIONS = 0;
+
+	/**
+	 * 各エラーコードに対応するメッセージ
+	 */
+	var errMsgMap = {};
+	errMsgMap[ERR_CODE_NO_COMPARE_FUNCTIONS] = '演算子"{0}"で"{1}"型の値を比較することはできません';
+
+	// メッセージの登録
+	addFwErrorCodeMap(errMsgMap);
 
 	// =============================
 	// Development Only
@@ -140,11 +230,12 @@
 		};
 	}
 
-	function createChangeListener(match, resultArray) {
+	function createChangeListener(match, compareFunc, resultArray) {
 		return function(ev) {
 			var removed = ev.removed;
 			var created = ev.created;
 			var changed = ev.changed;
+			var isSorted = true;
 
 			if (removed) {
 				for (var i = 0, l = removed.length; i < l; i++) {
@@ -167,6 +258,7 @@
 					// マッチしないかつ結果にあるものなら取り除く
 					if (match(changed[i].target.get())) {
 						if (resultIndex === -1) {
+							isSorted = false;
 							resultArray.push(changed[i].target);
 						}
 					} else {
@@ -182,10 +274,17 @@
 					// 新しく作成されたアイテムがあればマッチするかどうかチェックして
 					// マッチするなら結果に追加
 					if (match(created[i].get())) {
+						isSorted = false;
 						resultArray.push(created[i]);
 					}
 				}
 			}
+
+			// TODO ソートの仕様が決まったら実装する
+//			if (!isSorted) {
+//				// ソートする必要があるならソートする
+//				resultArray.sort(compareFunc);
+//			}
 		};
 	}
 
@@ -204,7 +303,7 @@
 				continue;
 			}
 			if ($.isPlainObject(criteria[prop])) {
-				// objectの場合は解析して追加
+				// objectの場合はネストしたcriteriaオブジェクトとして解析して追加する
 				nestedCriterias.push(compileCriteria(criteria[prop]));
 				continue;
 			}
@@ -213,14 +312,35 @@
 				userFunctions.push(criteria[prop]);
 				continue;
 			}
+			var queryValue = criteria[prop];
+
+			// queryValueのタイプをチェックする。配列指定なら先頭の要素でタイプを判定する
+			var valueForTypeCheck = isArray(queryValue) ? queryValue[0] : queryValue;
+			var compareFunctions = null;
+			if (valueForTypeCheck instanceof RegExp) {
+				// 正規表現の場合
+				compareFunctions = COMPARE_REGEXP_FUNCTIONS;
+			} else if (valueForTypeCheck instanceof Date) {
+				// Dateクラスの場合
+				compareFunctions = COMPARE_DATE_FUNCIONS;
+			} else {
+				// その他
+				compareFunctions = COMPARE_FUNCIONS;
+			}
 			// プロパティ名とオペランドに分割。連続した空白文字は無視
 			var tmp = $.trim(prop.replace(/ +/g, ' ')).split(' ');
 			// 演算子省略時は'='で比較
 			var op = tmp[1] || '=';
+			compareFunction = compareFunctions[op];
+			// compareFunctionが無い場合はエラー
+			if (!compareFunction) {
+				throwFwError(ERR_CODE_NO_COMPARE_FUNCTIONS, [op, $.type(valueForTypeCheck)]);
+			}
+
 			queries.push({
 				prop: tmp[0],
-				queryValue: criteria[prop],
-				compareFunction: COMPARE_FUNCIONS[op]
+				queryValue: queryValue,
+				compareFunction: compareFunction
 			});
 		}
 		compiledCriteria.match = criteria.__op === 'or' ? createORCompareFunction(compiledCriteria)
@@ -246,8 +366,13 @@
 		// criteriaを解析してresultArrayに結果を格納する関数を作成する
 		var compiledCriteria = compileCriteria(criteria);
 
+		// ソートするときの比較関数を作成する
+		function compareFunc() {
+		// TODO ソートの仕様が決まったら実装
+		}
+
 		// DataModelに変更があった時に結果を更新する関数をバインドする
-		var listener = createChangeListener(compiledCriteria.match, resultArray);
+		var listener = createChangeListener(compiledCriteria.match, compareFunc, resultArray);
 		this.addEventListener('itemsChange', listener);
 
 		// 今あるアイテムについて、条件を満たすものを列挙させる
