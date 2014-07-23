7ebb6070335348684027381e343e7cd5704fe3ff
fukudayasuo <fukuda.yasuo.2ga@jp.nssol.nssmc.com>
2014-07-23 11:10:26 +0900

    #373 名前空間解決のテストケースを修正しました

--- hifive/src/main/webapp/test/h5.res.js
+++ hifive/src/main/webapp/test/h5.res.js
@@ -49,37 +49,48 @@ $(function() {
 	// Definition
 	//=============================
 	module('名前空間の依存解決', {
+		setup: function() {
+			this.isFirst = false;
+			stop();
+			var that = this;
+			h5.res.require('h5resdata.controller.SampleController').resolve().done(
+					function(result) {
+						// 最初にロードした時の結果(=SampleController)を覚えておく
+						that.sampleController = result;
+						start();
+					});
+		},
 		teardown: function() {
 			deleteProperty(window, 'h5resdata');
 			// scriptタグの除去
 			$('script[src*="h5resdata\"]').remove();
-		}
+		},
+		sampleController: null
 	});
 
 	//=============================
 	// Body
 	//=============================
-	asyncTest('名前空間からjsファイルを取得', 2, function() {
-		h5.res.require('h5resdata.controller.SampleController').resolve().done(
-				function(result) {
-					strictEqual(h5resdata.controller.SampleController.__name,
-							'h5resdata.controller.SampleController', '指定した名前空間のコントローラが取得できること');
-					strictEqual(h5resdata.controller.SampleController, result,
-							'doneハンドラの引数にコントローラが渡されること');
-					start();
-				});
-	});
+	test('名前空間の解決', 4, function() {
+		strictEqual(h5resdata.controller.SampleController.__name,
+				'h5resdata.controller.SampleController', '指定した名前空間のコントローラが取得できること');
+		strictEqual(h5resdata.controller.SampleController, this.sampleController,
+				'doneハンドラの引数にコントローラが渡されること');
 
-	asyncTest('type:namespaceを指定', 2, function() {
 		h5.res.require('h5resdata.controller.SampleController', {
 			type: 'namespace'
 		}).resolve().done(
 				function(result) {
-					strictEqual(h5resdata.controller.SampleController.__name,
-							'h5resdata.controller.SampleController', '指定した名前空間のコントローラが取得できること');
 					strictEqual(h5resdata.controller.SampleController, result,
-							'doneハンドラの引数にコントローラが渡されること');
-					start();
+							'type:namespaceを指定した時、doneハンドラの引数にコントローラが渡されること');
+				});
+
+		h5resdata.controller.SampleController = 'hoge';
+
+		h5.res.require('h5resdata.controller.SampleController').resolve().done(
+				function() {
+					strictEqual(h5resdata.controller.SampleController, 'hoge',
+							'指定した名前空間に既に値が存在するとき、doneハンドラの引数に元々入っている値が渡されること');
 				});
 	});
 });
