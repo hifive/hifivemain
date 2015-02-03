ee722155b168f70a3cf40cf12928167cb177357f
simdy <shimoda.osamu@ns-sol.co.jp>
2015-02-04 08:48:03 +0900

    新しい仕様に基づいたh5.sceneの初期コードをコミット

--- hifive/src/main/webapp/src/h5.scene.js
+++ hifive/src/main/webapp/src/h5.scene.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 NS Solutions Corporation
+ * Copyright (C) 2014-2015 NS Solutions Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * hifive
  */
 
 /* ------ h5.scene ------ */
@@ -27,28 +26,13 @@
 	// =============================
 	// Production
 	// =============================
-	/** 吹き出し(三角の部分)の大きさ */
-	var ARROW_SIZE = 34;
-
-	/** オーバレイのクラス */
-	var CLASS_OVERLAY = 'h5PopupOverlay';
-
-	/** シーンの位置指定文字列 */
-	var POSITION_CONSTRAINT = {
-		top: 'top',
-		middle: 'middle',
-		bottom: 'bottom',
-		left: 'left',
-		center: 'center',
-		right: 'right'
-	};
 
 
 	// =============================
 	// Development Only
 	// =============================
 
-	//	var fwLogger = h5.log.createLogger('h5.scene');
+	var fwLogger = h5.log.createLogger('h5.scene');
 
 	/* del begin */
 	//	var FW_LOG_H5_WHEN_INVALID_PARAMETER = 'h5.scene.when: 引数にpromiseオブジェクトでないものが含まれています。';
@@ -75,309 +59,174 @@
 	// =============================
 	// Variables
 	// =============================
-	var scenesMap = {};
-
-	var sceneIdSeq = h5.core.data.createSequence(null, null, h5.core.data.SEQ_STRING);
 	// =============================
 	// Functions
 	// =============================
-	/**
-	 * アニメーションしながら表示する
-	 *
-	 * @param {jQuery} $element アニメーションさせる要素
-	 */
-	function animateShow($element) {
-		var height = $element.height();
-		$element.css({
-			height: 0,
-			display: 'block'
-		});
-		$element.addClass('inAnimation');
-		$element.animate({
-			height: height,
-			opacity: 1
-		}, "slow");
-		$element.promise().done(function() {
-			$element.removeClass('inAnimation');
-		});
+	function TypedMessage(type, data) {
+		this.type = type;
+		this.data = data;
 	}
 
-	/**
-	 * アニメーションしながら非表示にする
-	 *
-	 * @param {jQuery} $element アニメーションさせる要素
-	 */
-	function animateHide($element) {
-		$element.addClass('inAnimation');
-		$element.animate({
-			height: 0,
-			opacity: 0
-		}, "slow");
-		$element.promise().done(function() {
-			$element.removeClass('inAnimation');
-			$element.css('display', 'none');
-			$element.css('height', '');
-			removeOverlay();
-		});
-	}
+	function MessageChannel(type, targetWindow) {
+		this.type = type;
 
-	function showOverlay($root) {
-		// すでに表示済みなら何もしない
-		if ($('.' + CLASS_OVERLAY).length) {
-			return;
+		this._targetWindow = targetWindow;
+
+		this._closed = false;
+
+		var that = this;
+
+		this._recv = function(ev) {
+			that._receiveMessage(ev);
+		};
+
+		$.on(window, 'message', this._recv);
+	}
+	h5.mixin.eventDispatcher.mix(MessageChannel.prototype);
+	$.extend(MessageChannel.prototype, {
+		send: function(data) {
+			if (this._closed) {
+				//TODO fwErrorを投げる
+				alert('クローズ済みのチャネルです。送信できません。');
+				return;
+			}
+
+			var msg = new TypedMessage(this.type, data);
+
+			var msgSerialized = h5.u.obj.serialize(msg);
+
+			//TODO originを指定できるようにする, IE8-にoriginがないので対応が必要
+			this._targetWindow.postMessage(msgSerialized, location.origin);
+		},
+
+		close: function() {
+			this._closed = true;
+			this.off(window, 'message', this._recv);
+		},
+
+		_receiveMessage: function(event) {
+			try {
+				var msg = h5.u.obj.deserialize(event.data);
+			} catch (e) {
+				fwLogger.debug('メッセージをデシリアライズできませんでした。無視します。');
+			}
+
+			if (msg.type !== this.type) {
+				return;
+			}
+
+			var ev = {
+				type: 'message',
+				data: msg.data
+			};
+
+			this.dispatchEvent(ev);
 		}
-		var overlay = $('<div>').addClass(CLASS_OVERLAY);
-		// rootがwindowまたはdocumentならbodyに追加
-		($root[0] === window || $root[0] === document ? $(document.body) : $root).append(overlay);
-		function resizeOverlay() {
-			overlay.css({
-				width: $root.innerWidth(),
-				height: $root.innerHeight()
-			});
-		}
-		resizeOverlay();
-		$(window).bind('resize.h5overlay', resizeOverlay);
-		$(window).bind('orientationchange.h5overlay', resizeOverlay);
-	}
 
-	function removeOverlay() {
-		$(window).unbind('resize.h5overlay');
-		$(window).unbind('orientationchange.h5overlay');
-		$('.' + CLASS_OVERLAY).remove();
-	}
+	});
 
 	// =========================================================================
 	//
 	// Body
 	//
 	// =========================================================================
-	/**
-	 * Sceneクラス
-	 *
-	 * @param {String|DOM|jQuery} element
-	 * @param {String} type
-	 */
-	function Scene(element, type) {
-		var $element = $(element);
-
-		switch (type) {
-		case 'arrow':
-			// h5arrowboxでラップする
-			$element = $('<div class="h5arrowbox"></div>').append($element);
-			break;
-		}
-
-		$element.addClass('h5scene');
-		var sceneId = sceneIdSeq.next();
-		$element.data('h5scene-id', sceneId);
-		scenesMap[sceneId] = this;
-
-		/**
-		 * @memberOf Scene
-		 */
-		this.id = sceneId;
-
-		/**
-		 * @memberOf Scene
-		 */
-		this.$element = $element;
-
-		/**
-		 * @memberOf Scene
-		 */
-		this.$content = $element.find('.content');
-
-		/**
-		 * @memberOf Scene
-		 */
-		this.type = type;
+	function Scene() {
+	//scene
 	}
-	$.extend(Scene.prototype, {
-		show: function(param) {
-			this._showParam = param;
-			param = param || {};
-			this.hide(false);
 
-			// $rootは位置計算の起点となるエレメント。未指定の場合はwindow
-			var $root = $(param.rootElement || window);
-			var $element = this.$element;
-			var styles = {
-				position: 'absolute',
-				display: 'block'
-			};
+	function SceneContainer() {
+	//ignore
+	}
 
-			// rootElementがwindowまたはdocumentならbodyにappend
-			var $appendTarget = $root[0] === window || $root[0] === document ? $(document.body)
-					: $root;
-			$appendTarget.append($element);
+	function InvocationProxy() {
 
-			// 表示
-			$element.css(styles);
+	}
 
-			// ------------- 位置計算 ------------- //
-			// positionを見る
-			var position = param.position;
-			var topLeftStyle = {};
-			if (position) {
-				var top = position.top;
-				var left = position.left;
+	function RemoteWindow(url, windowName, features, isModal) {
+		var win = window.open(url, windowName, features);
 
-				// topがキーワード指定('top','middle','bottom'の場合
-				if (top === POSITION_CONSTRAINT.middle) {
-					top = $root.scrollTop() + ($root.height() - $element.outerHeight()) / 2;
-				} else if (top === POSITION_CONSTRAINT.top) {
-					top = $root.scrollTop();
-				} else if (top === POSITION_CONSTRAINT.bottom) {
-					top = $root.scrollTop() + $root.height() - $element.outerHeight();
-				} else {
-					// キーワード指定じゃない場合は$rootからの相対位置
-					top = $appendTarget.offset().top + parseFloat(top);
-				}
+		this.window = win;
 
-				// leftがキーワード指定('left','center','right'の場合
-				if (left === POSITION_CONSTRAINT.center) {
-					left = $root.scrollLeft() + ($root.width() - $element.outerWidth()) / 2;
-				} else if (left === POSITION_CONSTRAINT.left) {
-					left = $root.scrollLeft();
-				} else if (left === POSITION_CONSTRAINT.right) {
-					left = $root.scrollLeft() + $root.width() - $element.outerWidth();
-				} else {
-					// キーワード指定じゃない場合はappendしたルートからの相対位置
-					left = $appendTarget.offset().left + parseFloat(left);
-				}
-				topLeftStyle.top = top;
-				topLeftStyle.left = left;
-			}
+		this._isModal = false;
 
-			switch (this.type) {
-			case 'arrow':
-				// 吹き出しの向き設定
-				var dir = param.direction || 'down';
-				$element.removeClass('up down right left');
-				$element.addClass(dir);
-				// targetから表示位置を取得する
-				// positionまたはtargetはどちらかの指定が必須。
-				var $target = $(param.target);
-				if (!$target.length) {
-					// targetに何も指定されていないなら位置は計算しない
-					break;
-				}
+		this._parentBlocker = null;
 
-				// targetから位置を計算する。targetが指定されている場合はpositionの設定は無視。
-				var targetW = $target.outerWidth();
-				var targetH = $target.outerHeight();
-				topLeftStyle = $target.offset();
-				// $targetと$arrowboxの左上の位置を合わせる
-				if (dir === 'up' || dir === 'down') {
-					// 吹き出しの位置が$targetの真ん中に来るように合わせる
-					topLeftStyle.left += (targetW - $element.outerWidth()) / 2;
-					if (dir === 'up') {
-						// 吹き出し分だけ上に移動
-						topLeftStyle.top -= $element.outerHeight() + ARROW_SIZE;
-					} else {
-						// $target分だけ下に移動
-						topLeftStyle.top += targetH + ARROW_SIZE;
-					}
-				} else {
-					// 吹き出しの位置が$targetの真ん中に来るように合わせる
-					topLeftStyle.top += (targetH - $element.outerHeight()) / 2;
-					if (dir === 'left') {
-						// 吹き出し分だけ左に移動
-						topLeftStyle.left -= $element.outerWidth() + ARROW_SIZE;
-					} else {
-						// $target分だけ下に移動
-						topLeftStyle.left += targetW + ARROW_SIZE;
-					}
-				}
-				break;
-			default:
-			}
-			$element.css(topLeftStyle);
-			$element.removeClass('hidden');
+		this.setModal(isModal === true ? true : false);
 
-			if (param.animation !== false) {
-				// animationはデフォルトでtrue
-				animateShow($element);
-			}
+		this._watchChild();
+	}
+	$.extend(RemoteWindow.prototype, {
+		invoke: function(method, args) {
+			var dfd = h5.async.deferred();
 
-			if (param.modal) {
-				$element.css('z-index', 10001);
-				// オーバレイを表示
-				showOverlay($root);
-			}
-
-			this._displaying = true;
-			return $element.promise();
+			return dfd.promise();
 		},
-		hide: function(isAnimation) {
-			if (!this._displaying) {
+
+		getControllerProxy: function(selector) {
+			var proxy = new InvocationProxy();
+
+			return proxy;
+		},
+
+		setModal: function(value) {
+			if (this._isModal == value) {
 				return;
 			}
-			// TODO アニメーションするかどうか引数にとる
-			this.$element.stop(true, true);
-			if (isAnimation === false) {
-				this.$element.css('display', 'none');
+
+			var message = '子ウィンドウを開いている間は操作できません。';
+
+			if (this._isModal) {
+				//true -> false なので親ウィンドウのブロックを外す
+				this._parentBlocker.hide();
+				this._parentBlocker = null;
 			} else {
-				animateHide(this.$element);
+				//false -> trueなので親ウィンドウをブロックする
+				this._parentBlocker = h5.ui.indicator({
+					target: 'body',
+					block: true,
+					message: message
+				}).show();
 			}
-			this.$element.addClass('hidden');
-			this._displaying = false;
+
+			this._isModal = value;
 		},
-		setContent: function(content) {
-			this.$content.children().remove();
-			this.$content.append(content);
+
+		close: function() {
+			var dfd = h5.async.deferred();
+
+			this.window.close();
+
+			return dfd.promise();
 		},
-		remove: function() {
-			this.hide();
-			this.$element.remove();
-			delete scenesMap[this.id];
-			// nullifyする
-			for ( var p in this) {
-				this[p] = null;
+
+		_watchChild: function() {
+			var that = this;
+
+			//TODO setModal(false)のときは監視しないようにする
+			function watch() {
+				if (that.window.closed) {
+					clearInterval(tid);
+					that.setModal(false);
+				}
 			}
-		},
-		reflesh: function(param) {
-			if (!this._displaying) {
-				// 表示中じゃない場合は何もしない
-				return;
-			}
-			// showの時に渡されたパラメータにrefleshに渡されたパラメータをマージしてshowを呼び出す
-			this.show($.extend(this._showParam, param));
-		},
-		_animatePromise: null,
-		_showParam: null,
-		_displaying: false
+
+			var tid = setInterval(watch, 300);
+		}
 	});
 
-	/**
-	 * Sceneを作成する
-	 *
-	 * @memberOf h5.scene
-	 * @param {String|DOM|jQuery} element
-	 * @param {String} type
-	 * @returns {Scene}
-	 */
-	function createScene(element, type) {
-		return new Scene(element, type);
+	function openWindow(url, name, features, isModal, controllerName, param) {
+		var remote = new RemoteWindow(url, name, features, isModal);
+
+		return remote;
 	}
 
-	/**
-	 * SceneをIDから取得する
-	 */
-	function getSceneById(id) {
-		return scenesMap[id];
-	}
 
 	// =============================
 	// Expose to window
 	// =============================
-	/**
-	 * @namespace
-	 * @name scene
-	 * @memberOf h5
-	 */
+
 	h5.u.obj.expose('h5.scene', {
-		createScene: createScene,
-		getSceneById: getSceneById
+		openWindow: openWindow
 	});
+
 })();
