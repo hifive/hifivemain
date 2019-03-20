e579fda3ab494f0a3d39692ffbe7b1a74197e193
kai.tomomi <kai.tomomi.p9c@jp.nssol.nssmc.com>
2019-03-20 13:37:04 +0900

    #609 index.jsにあるコードをhifiveのコントローラの形に修正。input要素の幅を50pxにし、inline-blockで横並びにして表のように表示するように修正。

--- hifive/src/main/webapp/study/plenty-input/index.js
+++ hifive/src/main/webapp/study/plenty-input/index.js
@@ -1,37 +1,48 @@
 $(function() {
-	var num;
-	$("#btn1").click(function() {
-		 num = $("#num").val();
-		var input = "";
-		for (var i = 0; i < num; i++) {
-			input += "<input name='name_" + i + "' />";
-		}
-		$("#form").append(input);
-	});
+	var inputController = {
+		__name: 'inputController',
 
+		// バリデーションコントローラの設定
+		_formController: h5.ui.FormController,
+		__meta: {
+			_formController: {
+				rootElement: 'form'
+			}
+		},
 
-	$("#btn2").click(function() {
-		this.formController = h5.core.controller('.form', h5.ui.FormController);
-		var ruleNum = $("#rule").val();
+		'#btn1 click': function(context, $el) {
+			var inputNum = $("#num").val();
+			var input = "";
+			for (var i = 0; i < inputNum; i++) {
+				input += "<input class='plenty-input' name='name_" + i + "' />";
+			}
+			$("#form").append(input);
+		},
 
-		//各inputにルールを追加
-		for (var i = 0; i < ruleNum; i++) {
-			var name = "name_" + i;
+		'#btn2 click': function(context, $el) {
+			var ruleNum = $("#rule").val();
 
-			var ruleObj = {};
-			var validateObj = {
-				required: true
-			};
-			ruleObj[name] = validateObj;
-			this.formController.addRule(ruleObj);
-		}
+			//各inputにルールを追加
+			for (var i = 0; i < ruleNum; i++) {
+				var name = "name_" + i;
 
-		const startTime = performance.now(); // 開始時間
-		for (var i = 0; i < num; i++) {
-			this.formController.validate("name_" + i);
-		}
-		const endTime = performance.now(); // 終了時間
-		var time = endTime - startTime;
-		$("#output").text(time);
-	});
+				var ruleObj = {};
+				var validateObj = {
+					required: true
+				};
+				ruleObj[name] = validateObj;
+				this._formController.addRule(ruleObj);
+			}
+
+			var validateNum = $("#num").val();
+			var startTime = performance.now(); // 開始時間
+			for (var i = 0; i < validateNum; i++) {
+				this._formController.validate("name_" + i);
+			}
+			var endTime = performance.now(); // 終了時間
+			var pastTime = endTime - startTime;
+			$("#output").text(pastTime);
+		},
+	};
+	h5.core.controller(document.body, inputController);
 });
