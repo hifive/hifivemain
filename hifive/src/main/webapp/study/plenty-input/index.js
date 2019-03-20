084cfa59f96b1520adea6120596008e9651641a9
kai.tomomi <kai.tomomi.p9c@jp.nssol.nssmc.com>
2019-03-20 10:32:53 +0900

    #609 画面を開いてすぐではなく、値が入力されたらその数値分のinput要素を生成するように修正。画面を開いてすぐではなく、値が入力されたらその数値分のinput要素にバリデートルールを指定し、バリデートを行うように修正。バリデートにかかった時間を画面上に表示するように修正。

--- hifive/src/main/webapp/study/plenty-input/index.js
+++ hifive/src/main/webapp/study/plenty-input/index.js
@@ -1,15 +1,21 @@
 $(function() {
-		var $result = $("#form"), n = 2500;
+	var num;
+	$("#btn1").click(function() {
+		 num = $("#num").val();
 		var input = "";
-		for (var i = 0; i < n; i++) {
+		for (var i = 0; i < num; i++) {
 			input += "<input name='name_" + i + "' />";
 		}
-		$result.append(input);
+		$("#form").append(input);
+	});
 
+
+	$("#btn2").click(function() {
 		this.formController = h5.core.controller('.form', h5.ui.FormController);
+		var ruleNum = $("#rule").val();
 
 		//各inputにルールを追加
-   		for (var i = 0; i < n; i++) {
+		for (var i = 0; i < ruleNum; i++) {
 			var name = "name_" + i;
 
 			var ruleObj = {};
@@ -20,9 +26,12 @@ $(function() {
 			this.formController.addRule(ruleObj);
 		}
 
-		console.time('timer1');
-		for (var i = 0; i < n; i++) {
+		const startTime = performance.now(); // 開始時間
+		for (var i = 0; i < num; i++) {
 			this.formController.validate("name_" + i);
 		}
-		console.timeEnd('timer1'); // 計測時間出力
+		const endTime = performance.now(); // 終了時間
+		var time = endTime - startTime;
+		$("#output").text(time);
 	});
+});
